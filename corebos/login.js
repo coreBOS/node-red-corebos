d58c86c19341b40391c744493d6f780d3a60b199
Joe Bordes <joe@tsolucio.com>
2024-01-05 09:18:31 +0100

    fix(Login/Logout) change it completely

--- corebos/login.js
+++ corebos/login.js
@@ -4,96 +4,73 @@ module.exports = function(RED) {
     function coreBOSLoginNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.connected = false;
-
         let client = RED.nodes.getNode(config.server);
 
         if (!client) {
             return;
         }
-
-        node.init = async function init() {
-            node.status({fill:"yellow", shape:"ring", text:"Connecting"});
-
-            node.onConnect = function () {
-                node.connected = true;
-                node.status({fill:"green", shape:"ring", text:"Ready"});
-            }
-
-            node.onDisconnect = function () {
-                node.connected = false;
-                node.status({fill:"red", shape:"ring", text:"Offline"});
-            }
-
-            node.onRequestTimeout = function () {
-                node.status({fill:"red", shape:"ring", text:"Timeout"});
-            }
-
-            node.onError = function (e) {
-                node.error(e.message);
-                node.status({fill:"red", shape:"ring", text:"Error"});
-            }
-
-            node.onMessage = async function (topic, message) {
-                cblib.setURL(client.options.server);
-                var payload = new Object();
-                payload.topic = topic;
-                payload.payload = new Object();
-                payload.payload.msg = message;
-                payload.payload.userId = '';
-                payload.payload.sessionName = '';
-                try {
-                    await cblib.doLogin(client.options.username, client.options.accesskey, false);
-                    let sessinfo = cblib.getSession();
-                    payload.payload.userId = sessinfo.userId ? sessinfo.userId : '';
-                    payload.payload.sessionName = sessinfo.sessionName ? sessinfo.sessionName : '';
-                    node.status({fill:"green", shape:"ring", text:"Ready"});
-                } catch (err) {
-                    node.error(err)
-                }
-                return payload
-            }
-
-            let payload = await node.onMessage('login', 'login');
-            node.send(payload);
-
-            // we should listen to coreBOS events here and react accordingly
+        let cbhost = client.options.server;
+        let cbuser = client.options.username;
+        let cbakey = client.options.accesskey;
+        if (cbhost == '' || cbuser == '' || cbakey == '') {
+            return;
         }
 
-        node.init().catch(e => {
-            node.onError(e);
-        });
-
-        node.on('input', async function (msg) {
-            let ts = msg.payload;
-            msg.payload = new Object();
-            msg.payload.timestamp = ts;
-            let payload = await node.onMessage('login', 'input');
-            msg.payload.corebos = payload.payload;
-            node.send(msg);
-        });
-
-        node.on('close', function (done) {
-            node.consumer.disconnect().then(() => {
-                node.status({});
-                cblib.doLogout();
+        node.on('input', async function (msg, send, done) {
+            node.status({fill:"yellow", shape:"ring", text:"Executing"});
+            cblib.setConnection({'host': cbhost, 'sessionName': '', userId: false});
+            let cinfo = {};
+            cinfo.host = cbhost;
+            cinfo.userId = '';
+            cinfo.sessionName = '';
+            try {
+                await cblib.doLogin(cbuser, cbakey, false);
+                let sessinfo = cblib.getSession();
+                cinfo.userId = sessinfo.userId ? sessinfo.userId : '';
+                cinfo.sessionName = sessinfo.sessionName ? sessinfo.sessionName : '';
+                node.status({fill:"green", shape:"ring", text:"Ready"});
+            } catch (err) {
+                node.status({fill:"red", shape:"ring", text:"Error"});
+            }
+            if (typeof(msg.payload)=='undefined') {
+                msg.payload = {};
+            }
+            msg.payload.corebos = cinfo;
+            if (config.name != '') {
+                let cname = config.name.replace(/\s/g, '');
+                msg.payload[cname] = cinfo;
+            }
+            if (send) {
+                send(msg)
+            } else {
+                node.send(msg);
+            }
+            if (done) {
                 done();
-            }).catch(e => {
-                node.onError(e);
-            });
+            }
         });
+
+        this.on('close', function (done) {
+            node.status({});
+            cblib.doLogout();
+            done();
+        });
+
+        // we should listen to coreBOS events here and react accordingly
+        // [[Event emitter for function object in separate module - Developing Nodes - Node-RED Forum|https://discourse.nodered.org/t/event-emitter-for-function-object-in-separate-module/48850]]
     }
+
     RED.nodes.registerType('corebos-login', coreBOSLoginNode);
 
     RED.httpAdmin.post('/coreboslogin/:id', RED.auth.needsPermission('corebos.write'), function(req,res) {
-        var node = RED.nodes.getNode(req.params.id);
-        if (node != null) {
+        var cbnode = RED.nodes.getNode(req.params.id);
+        if (cbnode != null) {
             try {
-                node.receive();
+                cbnode.receive();
                 res.sendStatus(200);
             } catch(err) {
                 res.sendStatus(500);
-                node.error(RED._('corebos.loginfailed', {error:err.toString()}));
+                cbnode.error(RED._('corebos.loginfailed', {error:err.toString()}));
             }
         } else {
             res.sendStatus(404);
--- corebos/logout.js
+++ corebos/logout.js
@@ -4,76 +4,31 @@ module.exports = function(RED) {
     function coreBOSLogoutNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.connected = false;
 
-        let client = RED.nodes.getNode(config.server);
-
-        if (!client) {
-            return;
-        }
-
-        node.init = async function init() {
+        node.on('input', async function (msg, send, done) {
             node.status({fill:"yellow", shape:"ring", text:"Disconnecting"});
-
-            node.onConnect = function () {
-                node.connected = true;
+            let conn = config.connection == '' ? 'corebos' : config.connection;
+            if (conn in msg.payload && msg.payload[conn].sessionName != '') {
+                cblib.setConnection(msg.payload[conn]);
+                try {
+                    await cblib.doLogout();
+                    cblib.setConnection({'host': '', 'sessionName': '', userId: false});
+                    delete msg.payload[conn];
+                    if (send) {
+                        send(msg);
+                    } else {
+                        node.send(msg);
+                    }
+                } catch (err) {
+                    node.error(err);
+                }
                 node.status({fill:"green", shape:"ring", text:"Done"});
+            } else {
+                node.status({fill:"red", shape:"ring", text:"Login"});
             }
-
-            node.onDisconnect = function () {
-                node.connected = false;
-                node.status({fill:"red", shape:"ring", text:"Offline"});
-            }
-
-            node.onRequestTimeout = function () {
-                node.status({fill:"red", shape:"ring", text:"Timeout"});
-            }
-
-            node.onError = function (e) {
-                node.error(e.message);
-                node.status({fill:"red", shape:"ring", text:"Error"});
-            }
-
-            node.onMessage = async function (topic, message) {
-                cblib.setURL(client.options.server);
-                await cblib.doLogout();
-                var payload = new Object();
-                payload.topic = topic;
-                payload.payload = new Object();
-                payload.payload.msg = message;
-                payload.payload.userId= '';
-                payload.payload.sessionName = '';
-                node.status({fill:"green", shape:"ring", text:"Done"});
-                return payload
-            }
-
-            let payload = await node.onMessage('logout', 'logout');
-            node.send(payload);
-
-            // we should listen to coreBOS events here and react accordingly
-        }
-
-        node.init().catch(e => {
-            node.onError(e);
-        });
-
-        node.on('input', async function (msg) {
-            let ts = msg.payload;
-            msg.payload = new Object();
-            msg.payload.timestamp = ts;
-            let payload = await node.onMessage('logout', 'input');
-            msg.payload.corebos = payload.payload;
-            node.send(msg);
-        });
-
-        node.on('close', function (done) {
-            node.consumer.disconnect().then(() => {
-                node.status({});
-                cblib.doLogout();
+            if (done) {
                 done();
-            }).catch(e => {
-                node.onError(e);
-            });
+            }
         });
     }
     RED.nodes.registerType('corebos-logout', coreBOSLogoutNode);
